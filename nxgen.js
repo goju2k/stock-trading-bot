6c85181611dab6dcae519a961b968dc96a73e793
juyounggo <juyounggo@rsquare.co.kr>
2023-12-06 14:00:08 +0900

    [UPDATE] 모듈 구조 / lint 셋업

---
+++ nxgen.js
@@ -0,0 +1,20 @@
+const { exec } = require('child_process');
+const args = [];
+process.argv.forEach((a) => {
+  args.push(a);
+})
+
+const userArgs = args.splice(2)
+
+const libraryName = userArgs[0];
+if (!libraryName || libraryName === '') {
+  throw new Error('libraryName is needed (arg0)');
+}
+
+const libraryNameSplit = libraryName.split('/');
+
+const simpleName = libraryNameSplit[libraryNameSplit.length - 1];
+
+const command = `nx g @nx/react:library --name=${simpleName} --unitTestRunner=jest --bundler=vite --directory=${libraryName} --component=false --importPath=@${libraryName} --minimal=true --projectNameAndRootFormat=as-provided --simpleName=true`
+console.log('command', command);
+exec(command);
---
+++ shared/hooks/api-hook/README.md
@@ -0,0 +1,7 @@
+# api-hook
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test api-hook` to execute the unit tests via [Vitest](https://vitest.dev/).
