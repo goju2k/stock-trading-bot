baf4b70a713ea82ccbc0ee4819354b53427bb932
juyounggo <juyounggo@rsquare.co.kr>
2023-12-06 13:11:24 +0900

    [NEW] nx 프로젝트 초기화

---
+++ apps/stock-trading-bot/src/app/app.tsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+import NxWelcome from './nx-welcome';
+
+const StyledApp = styled.div`
+  // Your style here
+`;
+
+export function App() {
+  return (
+    <StyledApp>
+      <NxWelcome title="stock-trading-bot" />
+    </StyledApp>
+  );
+}
+
+export default App;
