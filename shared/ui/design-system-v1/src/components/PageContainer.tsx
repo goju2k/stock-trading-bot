cf5f5f6160cabd175c1a6d1ee91d4d700671e7ec
goju2k <goju2k@naver.com>
2023-12-27 16:14:57 +0900

    [FIX] 페이지 타이틀 변경되지 않는 문제 수정

--- shared/ui/design-system-v1/src/components/PageContainer.tsx
+++ shared/ui/design-system-v1/src/components/PageContainer.tsx
@@ -1,4 +1,4 @@
-import { PropsWithChildren, useEffect } from 'react';
+import { PropsWithChildren, useContext, useEffect, useRef } from 'react';
 import { useSetRecoilState } from 'recoil';
 import styled from 'styled-components';
 
@@ -7,6 +7,7 @@ import { Body } from './Body';
 import { Footer } from './Footer';
 import { Header } from './Header';
 
+import { ComponentRoutesContext, ComponentRoutesContextInterface } from '../routes';
 import { PageState } from '../states/page-state';
 
 const PAGE_PADDING = '10px';
@@ -19,13 +20,25 @@ export interface PageContainerProps {
   title: string;
 }
 export function PageContainer({ title, children }:PropsWithChildren<PageContainerProps>) {
+
+  const ref = useRef<HTMLDivElement|null>(null);
+
   const setPageState = useSetRecoilState(PageState);
   useEffect(() => {
-    setPageState({ title });
+    if (ref.current?.checkVisibility()) {
+      setPageState({ title });
+    }
   }, [ title ]);
 
+  const pathContext = useContext<ComponentRoutesContextInterface>(ComponentRoutesContext);
+  useEffect(() => {
+    if (ref.current?.checkVisibility()) {
+      setPageState({ title });
+    }
+  }, [ pathContext ]);
+
   return (
-    <PageWrapper flexGap={PAGE_PADDING} flexPadding={PAGE_PADDING}>
+    <PageWrapper ref={ref} flexGap={PAGE_PADDING} flexPadding={PAGE_PADDING}>
       <Header />
       <Body>{children}</Body>
       <Footer />
