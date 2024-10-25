499bf35916f42995cb093f1b85ebaab9104c89e4
goju2k <goju2k@naver.com>
2024-10-25 16:34:14 +0900

    테스트 빌드 2

--- apps/kis-server/src/main.mjs
+++ apps/kis-server/src/main.mjs
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(cors());
 
 app.get('/health', (req, res) => {
-  res.send('cd/cd ok !!!    ');
+  res.send('health ok !!!');
 });
 
 const target = 'openapi.koreainvestment.com:9443';
